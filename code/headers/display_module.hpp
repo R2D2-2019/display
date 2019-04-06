0a738efc941274182976ff0534f9a18ce1817fa1
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-04-06 16:41:13 +0200

    changed module to the new data packet

--- code/headers/display_module.hpp
+++ code/headers/display_module.hpp
@@ -1,19 +1,12 @@
 #pragma once
 
 #include <base_module.hpp>
+#include <hwlib.hpp>
 
 namespace r2d2::display {
     class module_c : public base_module_c {
     protected:
         hwlib::window &display;
-
-        /**
-         * This function converts a uin16_t 565 color to a hwlib color.
-         * 
-         */
-        hwlib::color to_color(uint16_t c) {
-            return hwlib::color(c >> 11, (c >> 5) & 0x3F, c & 0x1F);
-        }
 
     public:
         /**
@@ -25,7 +18,7 @@ namespace r2d2::display {
 
             // Set up listeners
             comm.listen_for_frames({
-                r2d2::frame_type::DISPLAY_RECTANGLE
+                r2d2::frame_type::DISPLAY_FILLED_RECTANGLE
             });
         }
 
@@ -43,12 +36,13 @@ namespace r2d2::display {
 
                 // Get the data from the frame
                 const auto data =
-                    frame.as_frame_type<frame_type::DISPLAY_RECTANGLE>();
+                    frame.as_frame_type<frame_type::DISPLAY_FILLED_RECTANGLE>();
 
                 for (uint8_t y = 0; y < data.height; y++) {
                     for (uint8_t x = 0; x < data.width; x++) {
-                        display.write(hwlib::xy(data.x + x, data.y + y),
-                                      to_color(data.color));
+                        display.write(
+                            hwlib::xy(data.x + x, data.y + y),
+                            hwlib::color(data.red, data.green, data.blue));
                     }
                 }
             }
