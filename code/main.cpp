adf552f345d88db1851748f793b2863874cb249b
Florian Humblot <florian.humblot@student.hu.nl>
2019-04-06 14:23:25 +0200

    Added comments to the test program

--- code/main.cpp
+++ code/main.cpp
@@ -7,18 +7,25 @@ int main(void) {
     // this starts the sysclock
     hwlib::wait_ms(20);
 
+    // Initialise a I2C bus at full speed (400Kbit/s)
     auto bus = r2d2::i2c::i2c_bus_c(
         r2d2::i2c::i2c_bus_c::interface::interface_0,  400000);
+    // Initialise the display using address 0x3C and the bus
     auto display = r2d2::display::ssd1306_oled_buffered(bus, 0x3c);
+    // Clear the noise of the display
     display.clear();
+    // Flush the changes to the buffer
     display.flush();
-
+    // Pixel check: turn each pixel on
     for (uint_fast16_t y = 0; y < 64; y++) {
         for (uint_fast16_t x = 0; x < 128; x++) {
             display.write(hwlib::xy(x, y), hwlib::white);
         }
     }
+    // Flush the new buffer to the display at once
     display.flush();
+    // Clear the display
     display.clear();
-    hwlib::graphics_random_circles(display);
+    // Play wouter's random lines demo
+    hwlib::graphics_random_lines(display);
 }
