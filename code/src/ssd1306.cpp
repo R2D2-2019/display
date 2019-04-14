89f94c45505d78f4a3910a3aa500f4fe1b69db6a
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-04-15 00:32:40 +0200

    added comments to the ssd1306 i2c class

--- code/src/ssd1306.cpp
+++ code/src/ssd1306.cpp
@@ -4,39 +4,56 @@
 namespace r2d2::display {
     ssd1306_i2c_c::ssd1306_i2c_c(r2d2::i2c::i2c_bus_c &bus,
                                  const uint8_t &address)
-        : bus(bus), address(address), cursor(255, 255) {}
+        : bus(bus), address(address), cursor(255, 255) {
+    }
 
     void ssd1306_i2c_c::command(ssd1306_command c) {
+        // create command packet
         uint8_t data[] = {ssd1306_cmd_prefix, (uint8_t)c};
+
+        // write command to the bus
         bus.write(address, data, sizeof(data) / sizeof(uint8_t));
     }
 
     void ssd1306_i2c_c::command(ssd1306_command c, uint8_t d0) {
+        // create command packet
         uint8_t data[] = {ssd1306_cmd_prefix, (uint8_t)c, ssd1306_cmd_prefix,
                           d0};
+
+        // write command to the bus
         bus.write(address, data, sizeof(data) / sizeof(uint8_t));
     }
 
     void ssd1306_i2c_c::command(ssd1306_command c, uint8_t d0, uint8_t d1) {
+        // create command packet
         uint8_t data[] = {ssd1306_cmd_prefix, (uint8_t)c,
                           ssd1306_cmd_prefix, d0,
                           ssd1306_cmd_prefix, d1};
+
+        // write command to the bus
         bus.write(address, data, sizeof(data) / sizeof(uint8_t));
     }
 
     void ssd1306_i2c_c::data(uint8_t d) {
+        // create data packet
         uint8_t data[] = {ssd1306_data_prefix, d};
+        
+        // write data to the bus
         bus.write(address, data, sizeof(data) / sizeof(uint8_t));
     }
     void ssd1306_i2c_c::pixels_byte_write(hwlib::xy location, uint8_t d) {
 
+        // check if we need to update the current cursor of the screen
         if (location != cursor) {
             command(ssd1306_command::column_addr, location.x, 127);
             command(ssd1306_command::page_addr, location.y, 7);
             cursor = location;
         }
 
+        // write data to the screen
         data(d);
-        cursor.x++;
+
+        // update the local cursor
+        cursor.x++; // TODO: cursor fallthrough
     }
 } // namespace r2d2::display
