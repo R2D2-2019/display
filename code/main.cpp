f2e415ef07d1cf317d91d8bd3b4ab9613b72c635
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-04-14 22:12:29 +0200

    added a display to the module to test the module

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,9 @@
 #include <hwlib.hpp>
 #include <comm.hpp>
 
+#include <i2c_bus.hpp>
+#include <ssd1306_oled_buffered.hpp>
+
 #include "display_module.hpp"
 
 int main() {
@@ -11,11 +14,18 @@ int main() {
 
     hwlib::wait_ms(10);
 
+    // Initialise a I2C bus at full speed (400Kbit/s)
+    auto bus = r2d2::i2c::i2c_bus_c(
+        r2d2::i2c::i2c_bus_c::interface::interface_0, 400000);
+
+    // Initialise the display using address 0x3C and the bus
+    auto display = r2d2::display::ssd1306_oled_buffered_c(bus, 0x3c);
+
     r2d2::comm_c comm;
 
-    // display::module_c module(comm);
+    display::module_c module(comm, display);
 
     for (;;) {
-        // module.process();
+        module.process();
     }
 }
