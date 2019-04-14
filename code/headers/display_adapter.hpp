5c0e8c540cebbc258a420507c86e44b518cfca46
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-04-15 00:07:42 +0200

    changed documentation for buffered version

--- code/headers/display_adapter.hpp
+++ code/headers/display_adapter.hpp
@@ -6,7 +6,7 @@ namespace r2d2::display {
     class display : public hwlib::window {
     protected:
         /**
-         * @brief write implementation for hwlib
+         * @brief Write implementation for hwlib
          *
          * @param pos
          * @param col
@@ -16,7 +16,7 @@ namespace r2d2::display {
         }
 
         /**
-         * @brief converts a hwlib::color to the pixel data for the screen with
+         * @brief Converts a hwlib::color to the pixel data for the screen with
          * a maximum of two bytes for every pixel
          *
          * @param c
@@ -30,7 +30,7 @@ namespace r2d2::display {
         }
 
         /**
-         * @brief Directly write a pixel to the screen
+         * @brief Write a pixel to the screen
          *
          * @param x
          * @param y
@@ -39,7 +39,7 @@ namespace r2d2::display {
         virtual void set_pixel(uint16_t x, uint16_t y, const uint16_t data) = 0;
 
         /**
-         * @brief Directly write multiple pixels to the screen
+         * @brief Write multiple pixels to the screen
          *
          * @param x
          * @param y
@@ -48,10 +48,10 @@ namespace r2d2::display {
          * @param data
          */
         virtual void set_pixels(uint16_t x, uint16_t y, uint16_t width,
-                                uint16_t height, const uint16_t *data) = 0;
+                                uint16_t height, const uint16_t *data);
 
         /**
-         * @brief Directly fill multiple pixels with the same color to the
+         * @brief Fill multiple pixels with the same color to the
          * screen
          *
          * @param x
@@ -61,10 +61,10 @@ namespace r2d2::display {
          * @param data
          */
         virtual void set_pixels(uint16_t x, uint16_t y, uint16_t width,
-                                uint16_t height, const uint16_t data) = 0;
+                                uint16_t height, const uint16_t data);
 
         /**
-         * @brief override for hwlib::window the class doesn't need to implement
+         * @brief Override for hwlib::window the class doesn't need to implement
          * a flush if not needed
          *
          */
