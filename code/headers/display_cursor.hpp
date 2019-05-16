10eb8899c23cde9b7bb5d436892ff62ddadb2679
Jens Bouman <jens.bouman@student.hu.nl>
2019-05-16 14:38:51 +0200

    Added documentation for the display_cursor class

--- code/headers/display_cursor.hpp
+++ code/headers/display_cursor.hpp
@@ -6,8 +6,10 @@
  */
 namespace r2d2::display {
     struct display_cursor{
+        // Cursor position x,y. (0,0) is upper left corner.
         uint8_t cursor_x = 0;
         uint8_t cursor_y = 0;
+        // The color in which this cursor draws.
         hwlib::color cursor_color = hwlib::color(0, 0, 0);
         
     };
