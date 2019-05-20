a75e0c4c57cd5c837db06107aef17c7d024f0592
lldiegon <lldiegon@gmail.com>
2019-05-20 17:15:29 +0200

    Initial commit

--- test/main.cpp
+++ test/main.cpp
@@ -2,3 +2,62 @@
 
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+
+TEST_CASE("Display shows the correct character", "[display_module]") {
+    // The bus itself doesn't take any constructor arguments
+    mock_comm_c mock_bus;
+
+    // hwlib pin_out_store is handy for tests, because it allows
+    // us to inspect the current state.
+    hwlib::pin_out_store out;
+
+    /*
+     * The Display module.
+     * The module constructor receives a base_comm_c& and an hwlib::pin_out&.
+     * As you can see, using the constructor to receive interfaces on your module really
+     * helps with testing and abstraction!
+     */
+    led::module_c module(mock_bus, out);
+
+    // Create a frame
+    auto frame = mock_bus.create_frame<
+        frame_type::DISPLAY_8x8_CHARACTER
+    >();
+    auto frame2 = mock_bus.create_frame<
+        frame_type::frame_display_filled_rectangle_s
+    >();
+    auto frame2 = mock_bus.create_frame<
+        frame_type::frame_display_8x8_character_via_cursor_s
+    >();
+
+    // Display should display a character
+    // "as_frame_type" interprets the frame as the struct that corresponds
+    // to the given enumeration value.
+    // 
+    // E.g. frame_type::ACTIVITY_LED_STATE is matched to frame_activity_led_state_s,
+    // so calling as_frame_type with this enumartion value will yield a frame_activity_led_state_s instance.
+    // 
+    // Please note that this data is not copied, it is a non-owning reference to the frame
+    // created earlier. 
+    frame.as_frame_type<frame_type::DISPLAY_8x8_CHARACTER>().x = 100;
+    frame.as_frame_type<frame_type::DISPLAY_8x8_CHARACTER>().y = 100;
+    
+    frame.as_frame_type<frame_type::DISPLAY_8x8_CHARACTER>().red = 100;
+    frame.as_frame_type<frame_type::DISPLAY_8x8_CHARACTER>().blue = 100;
+    frame.as_frame_type<frame_type::DISPLAY_8x8_CHARACTER>().green = 100;
+
+    frame.as_frame_type<frame_type::DISPLAY_8x8_CHARACTER>().characters[243] = [1, 50];
+    
+    // Actually place the frame on the bus, so the module
+    // can see and process it.
+    mock_bus.accept_frame(frame);
+
+    // Sanity check
+    REQUIRE(out.value == false);
+
+    // Manually call the process function on the module
+    // to allow it to process everything.
+    module.process();
+
+    REQUIRE(out.value == true);
+}
