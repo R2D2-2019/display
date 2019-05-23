a9fc1916c4dcd0ec64676bd4f705f3fdd163ff68
Bas van Rossem <bas.vanrossem@student.hu.nl>
2019-05-23 14:37:45 +0200

    Added inverted color decorator

---
+++ code/headers/st7735_inverted_color.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <display_adapter.hpp>
+#include <hwlib.hpp>
+#include <st7735.hpp>
+
+namespace r2d2::display {
+
+    template <std::size_t CursorCount, uint8_t DisplaySizeWidth,
+              uint8_t DisplaySizeHeight>
+    class st7735_inverted_color_c
+        : public st7735_c<CursorCount, DisplaySizeWidth, DisplaySizeHeight> {
+    protected:
+        st7735_inverted_color_c(hwlib::spi_bus &bus, hwlib::pin_out &cs,
+                                hwlib::pin_out &dc, hwlib::pin_out &reset)
+            : st7735_c<CursorCount, DisplaySizeWidth, DisplaySizeHeight>(
+                  bus, cs, dc, reset) {
+            this->init();
+            // display inversion on, memory direction control
+            this->write_command(this->_INVON, this->_MADCTL);
+            this->write_data(0xC8);
+            hwlib::wait_ms(20);
+        }
+    };
+} // namespace r2d2::display
