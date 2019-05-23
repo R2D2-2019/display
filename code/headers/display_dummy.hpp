0d6e09c409890b15f8ec9366066df9e8c25d8751
Jens Bouman <jens.bouman@student.hu.nl>
2019-05-23 14:16:57 +0200

    Added display dummy for testing purposes

---
+++ code/headers/display_dummy.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <hwlib.hpp>
+#include <display_cursor.hpp>
+
+namespace r2d2::display{
+
+    template <std::size_t CursorCount, uint8_t DisplaySizeWidth, uint8_t DisplaySizeHeight>
+    class display_dummy_c : public display_c<CursorCount, DisplaySizeWidth, DisplaySizeHeight>{
+        public:
+            display_dummy_c():
+                display_c<CursorCount, DisplaySizeWidth, DisplaySizeHeight>(hwlib::xy(DisplaySizeWidth, DisplaySizeHeight)){
+
+            }
+            /**
+             * @brief Converts a hwlib::color to the pixel data for the screen with
+             * a maximum of two bytes for every pixel
+             *
+             * @param c
+             */
+            virtual uint16_t color_to_pixel(const hwlib::color &c) override {
+                return 0;
+            };
+
+            /**
+             * @brief Write a pixel to the screen
+             *
+             * @param x
+             * @param y
+             * @param data
+             */
+            virtual void set_pixel(uint16_t x, uint16_t y, const uint16_t data) override {};
+    };
+}
