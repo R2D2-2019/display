e926d6944389bbbdb03db584e1523e10ebcaad4e
Jens Bouman <jens.bouman@student.hu.nl>
2019-05-27 14:38:59 +0200

    Add cursor manipulation out of bounds test, and cursor color test

--- test/main.cpp
+++ test/main.cpp
@@ -7,6 +7,10 @@
 #include <display_module.hpp>
 #include <hwlib.hpp>
 
+/*
+ * Tests the default initialization of the cursors.
+ * The cursor used is the open_cursor
+ */
 TEST_CASE("Default cursor initialization", "[cursor]") {
     // Dummy display does nothing in set_pixel
     r2d2::display::display_dummy_c<
@@ -20,6 +24,12 @@ TEST_CASE("Default cursor initialization", "[cursor]") {
     REQUIRE(curs.cursor_color == hwlib::color(0, 0, 0));
 }
 
+/*
+ * Testing the cursor position manipulation using the set_cursor_position frame
+ * The cursor used is the open_cursor
+ *
+ * The test will move the cursor to 50,75 and then to 125,30
+ */
 TEST_CASE("Manipulate cursor positon", "[cursor]") {
     // Dummy display does nothing in set_pixel
     r2d2::display::display_dummy_c<
@@ -27,107 +37,259 @@ TEST_CASE("Manipulate cursor positon", "[cursor]") {
         128, 160>
         test_display;
 
-    test_display.set_cursor_positon(
-        static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR), 50,
-        75);
-    auto curs = test_display.get_cursor(
-        static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
-    REQUIRE(curs.cursor_x == 50);
-    REQUIRE(curs.cursor_y == 75);
+    SECTION("To 50,75") {
+        test_display.set_cursor_positon(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR), 50,
+            75);
+
+        auto cursor_first = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
+        REQUIRE(cursor_first.cursor_x == 50);
+        REQUIRE(cursor_first.cursor_y == 75);
+    }
+
+    SECTION("To 125,30") {
+        test_display.set_cursor_positon(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR),
+            125, 30);
+        auto cursor_second = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
+        REQUIRE(cursor_second.cursor_x == 125);
+        REQUIRE(cursor_second.cursor_y == 30);
+    }
 }
 
-TEST_CASE("Manipulate cursor position through frame",
-          "[cursor, internal_communication]") {
-    // The bus itself doesn't take any constructor arguments
-    r2d2::mock_comm_c mock_bus;
-
-    // Dummy display does nothing in set_pixel
-    r2d2::display::display_dummy_c<
-        static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
-        128, 160>
-        test_display;
-    /*
-     * The Display module.
-     * The module constructor receives a base_comm_c& and an hwlib::pin_out&.
-     * As you can see, using the constructor to receive interfaces on your
-     * module really helps with testing and abstraction!
-     */
-    r2d2::display::module_c module(mock_bus, test_display);
-
-    // Create a frame
-    auto frame = mock_bus.create_frame<r2d2::frame_type::CURSOR_POSITION>(
-        {static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR), 
-        50,
-        75});
-
-    // Actually place the frame on the bus, so the module
-    // can see and process it.
-    mock_bus.accept_frame(frame);
-
-    // Manually call the process function on the module
-    // to allow it to process everything.
-    module.process();
-
-    auto curs = test_display.get_cursor(
-        static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
-
-    REQUIRE(curs.cursor_x == 50);
-    REQUIRE(curs.cursor_y == 75);
-}
-
+/*
+ * When a character is written, the cursor has to move 8 pixels in order to 
+ * write the new character. This means that the cursor position can be 
+ * calculated according to starting position and number of characters moved.
+ *
+ * When a character would overwrite the current bounds, the cursor won't move
+ * any further. This means that when a cursor will never go out of bounds.
+ */
 TEST_CASE("Manipulate cursor position through character writing",
           "[cursor, internal_communication]") {
     // The bus itself doesn't take any constructor arguments
     r2d2::mock_comm_c mock_bus;
 
+    constexpr uint8_t width = 128;
+    constexpr uint8_t height = 160;
+    // Dummy display does nothing in set_pixel
+    r2d2::display::display_dummy_c<
+        static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
+        width, height>
+        test_display;
+
+    r2d2::display::module_c module(mock_bus, test_display);
+
+    SECTION("Within bounds") {
+        constexpr uint8_t start_x = 50;
+        constexpr uint8_t start_y = 75;
+        constexpr uint8_t string_length = 5;
+        // Send the cursor to 50,75
+        auto frame_pos =
+            mock_bus.create_frame<r2d2::frame_type::CURSOR_POSITION>(
+                {static_cast<uint8_t>(
+                     r2d2::claimed_display_cursor::OPEN_CURSOR),
+                 start_x, start_y});
+
+        // Actually place the frame on the bus, so the module
+        // can see and process it.
+        mock_bus.accept_frame(frame_pos);
+
+        // Manually call the process function on the module
+        // to allow it to process everything.
+        module.process();
+
+        // Sanity check
+        auto corsor_pre = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
+
+        REQUIRE(corsor_pre.cursor_x == start_x);
+        REQUIRE(corsor_pre.cursor_y == start_y);
+
+        auto frame_char = mock_bus.create_frame<
+            r2d2::frame_type::DISPLAY_8X8_CHARACTER_VIA_CURSOR>(
+            {static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR),
+             {'T', 'e', 's', 't', '!'}});
+
+        mock_bus.accept_frame(frame_char);
+        module.process();
+
+        auto cursor_post = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
+
+        REQUIRE(cursor_post.cursor_x == start_x + (string_length * 8));
+        REQUIRE(cursor_post.cursor_y == start_y);
+    }
+
+    SECTION("Out of bounds") {
+        constexpr uint8_t start_x = 100;
+        constexpr uint8_t start_y = 75;
+
+        // Send the cursor to 50,75
+        auto frame_pos =
+            mock_bus.create_frame<r2d2::frame_type::CURSOR_POSITION>(
+                {static_cast<uint8_t>(
+                     r2d2::claimed_display_cursor::OPEN_CURSOR),
+                 start_x, start_y});
+
+        // Actually place the frame on the bus, so the module
+        // can see and process it.
+        mock_bus.accept_frame(frame_pos);
+
+        // Manually call the process function on the module
+        // to allow it to process everything.
+        module.process();
+
+        // Sanity check
+        auto cursor_pre = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
+
+        REQUIRE(cursor_pre.cursor_x == start_x);
+        REQUIRE(cursor_pre.cursor_y == start_y);
+
+        auto frame_char = mock_bus.create_frame<
+            r2d2::frame_type::DISPLAY_8X8_CHARACTER_VIA_CURSOR>(
+            {static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR),
+             {'T', 'e', 's', 't', '!'}});
+
+        mock_bus.accept_frame(frame_char);
+        module.process();
+
+        auto cursor_post = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
+
+        REQUIRE(cursor_post.cursor_x == (start_x + ((width - start_x) / 8) * 8));
+        REQUIRE(cursor_post.cursor_y == start_y);
+    }
+}
+
+/*
+ * The cursor also stores color, and thus has to be be able to be changed.
+ * The test will take a color, change it to red, green, blue and white. 
+ */
+
+TEST_CASE("Change cursor color", "[cursor, internal_communication]"){
+    // The bus itself doesn't take any constructor arguments
+    r2d2::mock_comm_c mock_bus;
+
     // Dummy display does nothing in set_pixel
     r2d2::display::display_dummy_c<
         static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
         128, 160>
         test_display;
-    /*
-     * The Display module.
-     * The module constructor receives a base_comm_c& and an hwlib::pin_out&.
-     * As you can see, using the constructor to receive interfaces on your
-     * module really helps with testing and abstraction!
-     */
+
     r2d2::display::module_c module(mock_bus, test_display);
+    SECTION("To red"){
+        uint8_t red = 255;
+        uint8_t green = 0;
+        uint8_t blue = 0;
+        // Change the color to red
+        auto frame_color =
+            mock_bus.create_frame<r2d2::frame_type::CURSOR_COLOR>(
+                {static_cast<uint8_t>(
+                     r2d2::claimed_display_cursor::OPEN_CURSOR),
+                 red, green, blue});
 
-    // Send the cursor to 50,75
-    auto frame_pos = mock_bus.create_frame<r2d2::frame_type::CURSOR_POSITION>(
-        {static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR), 
-        50,
-        75});
+        // Actually place the frame on the bus, so the module
+        // can see and process it.
+        mock_bus.accept_frame(frame_color);
 
-    // Actually place the frame on the bus, so the module
-    // can see and process it.
-    mock_bus.accept_frame(frame_pos);
+        // Manually call the process function on the module
+        // to allow it to process everything.
+        module.process();
+        // Sanity check
+        auto cursor = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
 
-    // Manually call the process function on the module
-    // to allow it to process everything.
-    module.process();
+        auto cursor_color = cursor.cursor_color;
+        REQUIRE(cursor_color.red == red);
+        REQUIRE(cursor_color.green == green);
+        REQUIRE(cursor_color.blue == blue);
+    }
 
-    auto curs_pre = test_display.get_cursor(
-        static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
+    SECTION("To green"){
+        uint8_t red = 0;
+        uint8_t green = 255;
+        uint8_t blue = 0;
+        // Change the color to red
+        auto frame_color =
+            mock_bus.create_frame<r2d2::frame_type::CURSOR_COLOR>(
+                {static_cast<uint8_t>(
+                     r2d2::claimed_display_cursor::OPEN_CURSOR),
+                 red, green, blue});
 
-    REQUIRE(curs_pre.cursor_x == 50);
-    REQUIRE(curs_pre.cursor_y == 75);
+        // Actually place the frame on the bus, so the module
+        // can see and process it.
+        mock_bus.accept_frame(frame_color);
 
-    auto frame_char = mock_bus.create_frame<
-            r2d2::frame_type::DISPLAY_8X8_CHARACTER_VIA_CURSOR
-        >({
-            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR),
-            "Test!"
-        });
+        // Manually call the process function on the module
+        // to allow it to process everything.
+        module.process();
+        // Sanity check
+        auto cursor = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
 
-    mock_bus.accept_frame(frame_char);
-    module.process();
+        auto cursor_color = cursor.cursor_color;
+        REQUIRE(cursor_color.red == red);
+        REQUIRE(cursor_color.green == green);
+        REQUIRE(cursor_color.blue == blue);
+    }
 
-    auto curs_post = test_display.get_cursor(
-        static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
+    SECTION("To blue"){
+        uint8_t red = 0;
+        uint8_t green = 0;
+        uint8_t blue = 255;
+        // Change the color to red
+        auto frame_color =
+            mock_bus.create_frame<r2d2::frame_type::CURSOR_COLOR>(
+                {static_cast<uint8_t>(
+                     r2d2::claimed_display_cursor::OPEN_CURSOR),
+                 red, green, blue});
 
-    REQUIRE(curs_post.cursor_x == 90);
-    REQUIRE(curs_post.cursor_y == 75);
+        // Actually place the frame on the bus, so the module
+        // can see and process it.
+        mock_bus.accept_frame(frame_color);
 
+        // Manually call the process function on the module
+        // to allow it to process everything.
+        module.process();
+        // Sanity check
+        auto cursor = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
 
+        auto cursor_color = cursor.cursor_color;
+        REQUIRE(cursor_color.red == red);
+        REQUIRE(cursor_color.green == green);
+        REQUIRE(cursor_color.blue == blue);
+    }
+
+    SECTION("To white"){
+        uint8_t red = 255;
+        uint8_t green = 255;
+        uint8_t blue = 255;
+        // Change the color to red
+        auto frame_color =
+            mock_bus.create_frame<r2d2::frame_type::CURSOR_COLOR>(
+                {static_cast<uint8_t>(
+                     r2d2::claimed_display_cursor::OPEN_CURSOR),
+                 red, green, blue});
+
+        // Actually place the frame on the bus, so the module
+        // can see and process it.
+        mock_bus.accept_frame(frame_color);
+
+        // Manually call the process function on the module
+        // to allow it to process everything.
+        module.process();
+        // Sanity check
+        auto cursor = test_display.get_cursor(
+            static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR));
+
+        auto cursor_color = cursor.cursor_color;
+        REQUIRE(cursor_color.red == red);
+        REQUIRE(cursor_color.green == green);
+        REQUIRE(cursor_color.blue == blue);
+    }
 }
