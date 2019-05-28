69b0f039ac19f4e697a8c5b1f87be76a3579da2b
BasvRossem <31800724+BasvRossem@users.noreply.github.com>
2019-05-28 13:09:16 +0200

    Added new main

--- code/main.cpp
+++ code/main.cpp
@@ -1,63 +1,37 @@
 #include <comm.hpp>
 #include <hwlib.hpp>
 
-#include "display_module.hpp"
+#include <display_module.hpp>
 #include <display_screen.hpp>
-#include <i2c_bus.hpp>
-#include <ssd1306_oled_unbuffered.hpp>
 #include <ssd1306_oled_buffered.hpp>
-#include <st7735_inverted_color_unbuffered.hpp>
-#include <st7735_unbuffered.hpp>
 
 int main() {
     // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
 
     hwlib::wait_ms(10);
-    /*=====================================================================
-    // hardware spi
-    auto spi_bus = due::hwspi(0, due::hwspi::SPI_MODE::SPI_MODE0, 3);
-    auto pin_dummy = hwlib::pin_out_dummy;
 
-    auto dc = hwlib::target::pin_out(hwlib::target::pins::d9);
-    auto rst = hwlib::target::pin_out(hwlib::target::pins::d8);
-
-    r2d2::display::st7735_inverted_color_unbuffered_c<
-        static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
-        r2d2::display::st7735_128x160_s
-        > color_display(spi_bus, pin_dummy, dc, rst);
-
-    r2d2::comm_c comm;
-
-    r2d2::display::module_c<
-            static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
-            r2d2::display::st7735_128x160_s
-        > module(comm, color_display);
-    ==========================================================================*/
-
-    auto scl = hwlib::target::pin_oc(hwlib::target::pins::scl1);
-    auto sda = hwlib::target::pin_oc(hwlib::target::pins::sda1);
-
-    // create the bus we want to use
+    // Initialise pins
+    auto scl = hwlib::target::pin_oc(hwlib::target::pins::scl);
+    auto sda = hwlib::target::pin_oc(hwlib::target::pins::sda);
+    
+    // Initialise i2c_bus
     using i2c_bus = r2d2::i2c::i2c_bus_c;
     i2c_bus bus(i2c_bus::interface::interface_1, 100'000);
 
-    // use pin_dummy becouse chip select(cs) is controlled by the hardware spi
+    // Initialise display
     r2d2::display::ssd1306_oled_buffered_c<
             static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
             r2d2::display::ssd1306_128x64_s> 
-        oled_display(bus, 0x3C);
+        display(bus, 0x3C);
 
     r2d2::comm_c comm;
 
     r2d2::display::module_c<
             static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
             r2d2::display::ssd1306_128x64_s> 
-        module(comm, oled_display);
+        module(comm, display);
     
-    
-    oled_display.clear();
-
     for (;;) {
         module.process();
     }
