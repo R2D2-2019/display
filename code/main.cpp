bb30dc69b63395f60558cdc957995c4bda906d86
Jens Bouman <jens.bouman@student.hu.nl>
2019-05-28 19:16:45 +0200

    Changed main to work with st7735 color screen

--- code/main.cpp
+++ code/main.cpp
@@ -2,34 +2,36 @@
 #include <hwlib.hpp>
 
 #include <display_module.hpp>
-#include <ssd1306_oled_buffered.hpp>
+#include <st7735_buffered.hpp>
 
 int main() {
     // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
 
     hwlib::wait_ms(10);
-
-    // Initialise pins
-    auto scl = hwlib::target::pin_oc(hwlib::target::pins::scl);
-    auto sda = hwlib::target::pin_oc(hwlib::target::pins::sda);
     
-    // Initialise i2c_bus
-    using i2c_bus = r2d2::i2c::i2c_bus_c;
-    i2c_bus bus(i2c_bus::interface::interface_1, 100'000);
+    // hardware spi
+    auto spi_bus = due::hwspi(0, due::hwspi::SPI_MODE::SPI_MODE0, 3);
+    auto pin_dummy = hwlib::pin_out_dummy;
 
-    // Initialise display
-    r2d2::display::ssd1306_oled_buffered_c<
-            static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
-            128, 64> 
-        display(bus, 0x3C);
+    auto dc = hwlib::target::pin_out(hwlib::target::pins::d9);
+    auto rst = hwlib::target::pin_out(hwlib::target::pins::d8);
+
+    // use pin_dummy becouse chip select(cs) is controlled by the hardware spi
+    r2d2::display::st7735_buffered_c<
+        static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
+        128, 160>
+        color_display(spi_bus, pin_dummy, dc, rst);
 
     r2d2::comm_c comm;
 
     r2d2::display::module_c<
             static_cast<std::size_t>(r2d2::claimed_display_cursor::CURSORS_COUNT),
-            128,64> 
-        module(comm, display);
+            128,160> 
+        module(comm, color_display);
+
+    color_display.clear();
+    color_display.flush();
 
     for (;;) {
         module.process();
