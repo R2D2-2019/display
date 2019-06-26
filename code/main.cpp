0092550cfba8846c2a829d45b77c6b91734e2f9d
Ramon <ramon.vanbemmel@student.hu.nl>
2019-06-26 15:27:42 +0200

    First attempt to print a char
    This is none working code

--- code/main.cpp
+++ code/main.cpp
@@ -2,42 +2,56 @@
 #include <hwlib.hpp>
 
 #include <display_module.hpp>
+#include <display_screen.hpp>
 #include <st7735_buffered.hpp>
-#include <st7735_unbuffered.hpp>
 #include <st7735_inverted_color_buffered.hpp>
 #include <st7735_inverted_color_unbuffered.hpp>
-#include <display_screen.hpp>
-#include <ssd1306_oled_buffered.hpp>
-#include <ssd1306_oled_unbuffered.hpp>
+#include <st7735_unbuffered.hpp>
+//#include <ssd1306_oled_buffered.hpp>
+//#include <ssd1306_oled_unbuffered.hpp>
 
 int main() {
     // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
 
     hwlib::wait_ms(10);
-    
+
     // hardware spi
     auto spi_bus = due::hwspi(0, due::hwspi::SPI_MODE::SPI_MODE0, 3);
     auto pin_dummy = hwlib::pin_out_dummy;
     auto dc = hwlib::target::pin_out(hwlib::target::pins::d9);
     auto rst = hwlib::target::pin_out(hwlib::target::pins::d8);
 
-
     // use pin_dummy because chip select(cs) is controlled by the hardware spi
-    r2d2::display::st7735_buffered_c<
-        r2d2::display::st7735_128x160_s>
+    r2d2::display::st7735_buffered_c<r2d2::display::st7735_128x160_s>
         color_display(spi_bus, pin_dummy, dc, rst);
 
     r2d2::comm_c comm;
 
-    r2d2::display::module_c<
-            r2d2::display::st7735_128x160_s> 
-        module(comm, color_display);
+    r2d2::display::module_c<r2d2::display::st7735_128x160_s> module(
+        comm, color_display);
 
     color_display.clear();
-    color_display.flush();
+    // color_display.flush();
+    color_display.set_cursor_color(
+        static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR),
+        hwlib::red);
 
-    for (;;) {
-        module.process();
-    }
+    // Set position of a claimed cursor to x = 15, y = 50
+    color_display.set_cursor_position(
+        static_cast<uint8_t>(r2d2::claimed_display_cursor::OPEN_CURSOR), 15,
+        50);
+    color_display.set_character(0, 0, 'H', color_display.color_to_pixel(hwlib::red));
+    color_display.flush();
+    //for (;;) {
+        // color_display.clear();
+        // display.set_cursor_position(
+        //     static_cast<uint8_t>(claimed_display_cursor::YOUR_CURSOR), 15,
+        //     50);
+        // display.set_character(0, 0, 'H', display.color_to_pixel(hwlib::red));
+        // color_display.flush();
+        // hwlib::wait_ms(1000);
+
+        // module.process();
+   // }
 }
